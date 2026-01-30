b6a24182d73c16208ce4c7d5ec5cfe342a7f1fe1
kell0wj <kello1000@gmail.com>
2026-01-30 01:19:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const Hours = document.getElementById('hours');
 const Minutes = document.getElementById('minutes');
 const Seconds = document.getElementById('seconds');
 
-const targetDate = new Date("November 1 2024 00:00:00").getTime();
+const targetDate = new Date("February 14 2026 00:00:00").getTime();
 
 function timer () {
     const currentDate = new Date().getTime();
